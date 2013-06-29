b882044818c7dd6bacbc5fc28395b9d284e865e3
Baris Ozcanli <barisozcanli@gmail.com>
2013-06-30 00:40:44 +0300

    adds constructor for setting the plateau in constructor

--- src/main/java/com/n11/rovers/models/RoverKAFA500.java
+++ src/main/java/com/n11/rovers/models/RoverKAFA500.java
@@ -1,10 +1,18 @@
 package com.n11.rovers.models;
 
+import com.n11.rovers.listener.Listener;
+
 public class RoverKAFA500 extends Rover {
 
     public RoverKAFA500(Coordinates location, Direction heading) {
         super();
         this.setLocation(location);
         this.setHeading(heading);
+    }
+    public RoverKAFA500(Coordinates location, Direction heading, Listener plateau) {
+        super();
+        this.setLocation(location);
+        this.setHeading(heading);
+        this.addListener(plateau);
     }
 }
