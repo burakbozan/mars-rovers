4d2b53a6392c2c8cc5752134852936b277612172
burakbozan <jeagle_1907@hotmail.com>
2013-12-16 14:36:24 +0200

    Update Command.java
    deneme

--- src/main/java/com/blackburn/rovers/commands/Command.java
+++ src/main/java/com/blackburn/rovers/commands/Command.java
@@ -3,5 +3,5 @@ package com.blackburn.rovers.commands;
 import com.blackburn.rovers.exception.RoverNotSetException;
 
 public interface Command {
-    void execute() throws RoverNotSetException;
+    void execute() throws RoverNotSetException; //yersen ;)
 }
